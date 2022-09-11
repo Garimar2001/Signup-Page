d1b9decfc2dcf9f373279ba673fc633d12142997
Garima Rawat <garimarawat348@gmail.com>
2022-09-11 17:50:58 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+// const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res)
+{
+
+    res.sendFile(__dirname+ "/signup.html");
+
+});
+
+app.post("/", function(req, res)
+{
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+
+
+                }
+
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us14.api.mailchimp.com/3.0/lists/477d4729e2"
+
+    const options = {
+        method: "POST",
+        auth: "garima:7cb2efff36f5204a99877a39e7fd42e7-us14"
+    }
+
+    const request = https.request(url, options, function(response)
+    {
+        if(response.statusCode === 200)
+        {
+            res.sendFile(__dirname + "/success.html");
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data){
+            // console.log.apply(JSON.parse(data));
+            console.log(JSON.parse(data));
+        })
+
+    })
+
+    request.write(jsonData);
+    request.end();
+
+
+
+
+});
+
+app.post("/failure", function(req,res)
+{
+    res.redirect("/")
+})
+
+app.listen(process.env.PORT || 3000, function()
+{
+    console.log("Server is running");
+});
+
+// 7cb2efff36f5204a99877a39e7fd42e7-us14
+// 477d4729e2
